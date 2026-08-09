852e32553ca55487bb24ba304f759230cfe4747d
58855krar-cloud <58855krar@gmail.com>
2026-08-09 19:46:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ import time
 import random
 import json
 import sys
+from telethon import TelegramClient, events
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = "8958079044:AAGuF319LFTe3hQymkw-T4iPFkIPVqA-XVU"
+
 OWNER_ID = 8345875922
 OWNER_USERNAME = "@UE_SH"
+API_ID = 26521501
+API_HASH = "8f3812eebeeb030fb8d4b3dfd9f1e16f"
+
+bot = TelegramClient('bot_session', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
 # ============================================================
 
 # ============================================================
