6cc08582d883ea3ff820c85eb8708693f132a1e5
58855krar-cloud <58855krar@gmail.com>
2026-08-09 21:09:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ import time
 import random
 import json
 import sys
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+Thread(target=run).start()
+
 from telethon import TelegramClient, events
 
 from telegram import Update
